1fa3adacc84793d62f3fd6a237f490bb06ae8fbe
aakash <ay9675928687@gmail.com>
2023-07-02 01:57:43 +0530

    added some initial files

---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Note = () => {
+  return (
+    <div className='note'>
+        <h1>
+            This is the title of the web page.
+        </h1>
+        <p>
+            This is the content of the page.
+        </p>
+    </div>
+  )
+}
+
+export default Note
